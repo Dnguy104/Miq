884b955ca0cb2f4b539f1bae0bc73e7c5ab578c7
unknown <danielnguyen441@gmail.com>
2019-03-24 14:48:43 -0700

    fixed test data

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -4,52 +4,52 @@ import Sidebar from '../components/Sidebar';
 
 type Props = {};
 
-  // [
-  //   {
-  //     name: "test",
-  //     [
-  //       key: "asdfgfdsgfdsh",
-  //       key1: "asdfgfdsgfdsh",
-  //       key2: "asdfgfdsgfdsh",
-  //       key3: "asdfgfdsgfdsh",
-  //       key4: "asdfgfdsgfdsh",
-  //       key5: "asdfgfdsgfdsh"
-  //     ]
-  //   },
-  //   {
-  //     name: "test",
-  //     [
-  //       key: "asdfgfdsgfdsh",
-  //       key1: "asdfgfdsgfdsh",
-  //       key2: "asdfgfdsgfdsh",
-  //       key3: "asdfgfdsgfdsh",
-  //       key4: "asdfgfdsgfdsh",
-  //       key5: "asdfgfdsgfdsh"
-  //     ]
-  //   },
-  //   {
-  //     name: "test",
-  //     [
-  //       key: "asdfgfdsgfdsh",
-  //       key1: "asdfgfdsgfdsh",
-  //       key2: "asdfgfdsgfdsh",
-  //       key3: "asdfgfdsgfdsh",
-  //       key4: "asdfgfdsgfdsh",
-  //       key5: "asdfgfdsgfdsh"
-  //     ]
-  //   },
-  //   {
-  //     name: "test",
-  //     [
-  //       key: "asdfgfdsgfdsh",
-  //       key1: "asdfgfdsgfdsh",
-  //       key2: "asdfgfdsgfdsh",
-  //       key3: "asdfgfdsgfdsh",
-  //       key4: "asdfgfdsgfdsh",
-  //       key5: "asdfgfdsgfdsh"
-  //     ]
-  //   }
-  // ]
+  [
+    {
+      name: "test",
+      {
+        key: "asdfgfdsgfdsh",
+        key1: "asdfgfdsgfdsh",
+        key2: "asdfgfdsgfdsh",
+        key3: "asdfgfdsgfdsh",
+        key4: "asdfgfdsgfdsh",
+        key5: "asdfgfdsgfdsh"
+      }
+    },
+    {
+      name: "test",
+      {
+        key: "asdfgfdsgfdsh",
+        key1: "asdfgfdsgfdsh",
+        key2: "asdfgfdsgfdsh",
+        key3: "asdfgfdsgfdsh",
+        key4: "asdfgfdsgfdsh",
+        key5: "asdfgfdsgfdsh"
+      }
+    },
+    {
+      name: "test",
+      {
+        key: "asdfgfdsgfdsh",
+        key1: "asdfgfdsgfdsh",
+        key2: "asdfgfdsgfdsh",
+        key3: "asdfgfdsgfdsh",
+        key4: "asdfgfdsgfdsh",
+        key5: "asdfgfdsgfdsh"
+      }
+    },
+    {
+      name: "test",
+      {
+        key: "asdfgfdsgfdsh",
+        key1: "asdfgfdsgfdsh",
+        key2: "asdfgfdsgfdsh",
+        key3: "asdfgfdsgfdsh",
+        key4: "asdfgfdsgfdsh",
+        key5: "asdfgfdsgfdsh"
+      }
+    }
+  ]
 
 
 export default class HomePage extends Component<Props> {
